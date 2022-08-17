90182929999831981cfd457ed86575537a69a77f
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-17 13:54:23 +0630

    Creating Post

--- lesson-2/src/App.js
+++ lesson-2/src/App.js
@@ -1,8 +1,10 @@
 
+import { AddPostForm } from './features/post/AddPostForm';
 import PostsList from './features/post/PostsList';
 function App() {
   return (
     <main className="App">
+        <AddPostForm/>
         <PostsList/>
     </main>
   );
