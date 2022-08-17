b8d7cccfbcc5b015dff6f3eaf2cebe199895b24d
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-17 18:16:42 +0630

    Editing the Post

--- lesson-4/src/App.js
+++ lesson-4/src/App.js
@@ -4,6 +4,7 @@ import PostsList from './features/post/PostsList';
 import Layout from "./components/Layout";
 import { Routes, Route } from 'react-router-dom';
 import SinglePostPage from './features/post/SinglePostPage';
+import EditPostForm from './features/post/EditPostForm';
 function App() {
   return (
     <Routes>
@@ -14,6 +15,7 @@ function App() {
         <Route path="post">
           <Route index element={<AddPostForm />} />
           <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
 
       </Route>
