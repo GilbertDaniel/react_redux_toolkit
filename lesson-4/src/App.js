b5eacc74938c43dfe7bdc3285b8a2e8925af2dc8
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-17 17:54:09 +0630

    Implementing Routes,Layout and PostDetail

--- lesson-4/src/App.js
+++ lesson-4/src/App.js
@@ -1,12 +1,23 @@
 
 import { AddPostForm } from './features/post/AddPostForm';
 import PostsList from './features/post/PostsList';
+import Layout from "./components/Layout";
+import { Routes, Route } from 'react-router-dom';
+import SinglePostPage from './features/post/SinglePostPage';
 function App() {
   return (
-    <main className="App">
-        <AddPostForm/>
-        <PostsList/>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+        </Route>
+
+      </Route>
+    </Routes>
   );
 }
 
