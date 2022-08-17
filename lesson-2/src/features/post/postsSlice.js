3a562fc601991a0e4c47c60501cf534fe500a2a6
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-17 13:27:35 +0630

    Implement PostAdded Action

--- lesson-2/src/features/post/postsSlice.js
+++ lesson-2/src/features/post/postsSlice.js
@@ -17,10 +17,16 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+        },
     }
 })
 
 export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
