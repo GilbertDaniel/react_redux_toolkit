c191c9eba1d5e9a706dcc1afa92bb4dafe74f4fb
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-20 13:10:50 +0630

    Implementing CRUD for Todo

--- lesson-6/src/api/apiSlice.js
+++ lesson-6/src/api/apiSlice.js
@@ -3,9 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3500'}),
+    tagTypes: ['Todos'],
     endpoints: (builder)=>({
         getTodos: builder.query({
             query: () => '/todos',
+            transformResponse: res => res.sort((a, b) => b.id - a.id),// order by desc for TodoList
+            providesTags: ['Todos']
         }),
         addTodo: builder.mutation({
             query: (todo) => ({
--- lesson-6/src/features/todos/TodoList.js
+++ lesson-6/src/features/todos/TodoList.js
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
-import { useGetTodosQuery } from '../../api/apiSlice'
+import {
+    useGetTodosQuery,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation,
+    useAddTodoMutation
+} from "../../api/apiSlice"
 
 const TodoList = () => {
     const [newTodo, setNewTodo] = useState('')
@@ -14,9 +19,13 @@ const TodoList = () => {
         error
     } = useGetTodosQuery()
 
+    const [addTodo] = useAddTodoMutation()
+    const [updateTodo] = useUpdateTodoMutation()
+    const [deleteTodo] = useDeleteTodoMutation()
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        //addTodo
+        addTodo({ userId: 1, title: newTodo, completed: false })
         setNewTodo('')
     }
 
@@ -43,7 +52,24 @@ const TodoList = () => {
     if(isLoading){
         content = <p>Loading...</p>
     }else if (isSuccess){
-        content = JSON.stringify(todos)
+        content = todos.map(todo => { //JSON.stringify(todos)
+            return (
+                <article key={todo.id}>
+                    <div className="todo">
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            id={todo.id}
+                            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </article>
+            )
+        })
     }else if(isError){
         content = <p>{error}</p>
     }
