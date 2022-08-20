45713ab3a4ea99ad484dac787ad55c3fcf47f711
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-20 10:42:12 +0630

    Initinal Commit

---
+++ lesson-5/src/App.js
@@ -0,0 +1,26 @@
+
+import { AddPostForm } from './features/post/AddPostForm';
+import PostsList from './features/post/PostsList';
+import Layout from "./components/Layout";
+import { Routes, Route } from 'react-router-dom';
+import SinglePostPage from './features/post/SinglePostPage';
+import EditPostForm from './features/post/EditPostForm';
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
