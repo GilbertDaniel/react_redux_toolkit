16888cc02ae619cb038bfbf39cfd856d198bc5fe
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-20 11:54:51 +0630

    Implementing UserPage

--- lesson-5/src/App.js
+++ lesson-5/src/App.js
@@ -2,9 +2,11 @@
 import { AddPostForm } from './features/post/AddPostForm';
 import PostsList from './features/post/PostsList';
 import Layout from "./components/Layout";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import SinglePostPage from './features/post/SinglePostPage';
 import EditPostForm from './features/post/EditPostForm';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
 function App() {
   return (
     <Routes>
@@ -17,6 +19,14 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* Catch all - replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
 
       </Route>
     </Routes>
